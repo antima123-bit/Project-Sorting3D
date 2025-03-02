694539701b7dc8ca3f61b9e16270c7c704a9b929
Antima Mishra <140072380+antima123-bit@users.noreply.github.com>
2025-03-02 08:18:32 +0530

    Update audio.js

--- audio.js
+++ audio.js
@@ -1,3 +1,9 @@
+/**
+ * Sorting Algorithms for Visualizer
+ * Author: Antima Mishra
+ * Date: March 2025
+ * Custom implementation of sorting with animations
+ */
 class AudioManager {
     constructor() {
         this.audioContext = new (window.AudioContext || window.webkitAudioContext)();
